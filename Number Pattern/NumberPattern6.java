8ed8509c971472f7398f2a9ed73d18808a932e1b
OmkarVanjare <131974759+OmkarVanjare@users.noreply.github.com>
2023-09-04 09:50:43 +0530

    NumberPattern6

---
+++ Number Pattern/NumberPattern6.java
@@ -0,0 +1,22 @@
+package NumberPattern;
+
+public class NumberPattern6 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			int num=i;
+			for(int j=1;j<=i;j++) {
+				System.out.print(num+" ");
+				num = 5+num-j;
+			}
+			System.out.println();
+		}
+//		1 
+//		2 6 
+//		3 7 10 
+//		4 8 11 13 
+//		5 9 12 14 15 
+
+	}
+
+}
